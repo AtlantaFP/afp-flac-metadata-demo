e244939bcecd4ae5151a90bcd04ae0a3f1f5cf4f
Ram Vedam <rvedam@gmail.com>
2020-06-05 18:59:57 -0600

    Fixed formatting for application

--- src/main.lisp
+++ src/main.lisp
@@ -22,51 +22,88 @@
 
 (defun open-flac-file ()
   (let ((filename
-	  (ltk:get-open-file :filetypes '(("FLaC" "*.flac")("All Files" "*")))))
-    (setf *current-metadata* (read-flac-file filename))
+          (ltk:get-open-file :filetypes '(("FLaC" "*.flac")("All Files" "*")))))
+    (unless (directory filename)
+     (setf *current-metadata* (read-flac-file filename)))
     filename))
 
 (defun main ()
   (ltk:with-ltk ()
-    (let* ((top-frame (make-instance 'ltk:frame))
-	   (button-frame (make-instance 'ltk:frame :master top-frame))
-	   (open-flac-file-button (make-instance 'ltk:button
-						 :master button-frame
-						 :text "Open Flac File"
-						 :command (lambda ()
-							    (ltk:wm-title ltk:*tk*
-									  (format nil "FLaC Metadata Viewer: ~A"
-										  (open-flac-file))))))
-	   (exit-button (make-instance 'ltk:button
-				       :master button-frame
-				       :text "Exit Application"
-				       :command (lambda ()
-						  (return-from main nil))))
-	   (details-pane (make-instance 'ltk:paned-window
-					:master top-frame))
-	   (stream-info-section-title (make-instance 'ltk:label
-						     :master details-pane
-						     :text "FLac File Stream Header Information"))
-	   (bits-per-sample-label (make-instance 'ltk:label
-						 :master details-pane
-						 :text "bits per sample:"))
-	   (bps-info (make-instance 'ltk:text
-				    :master details-pane
-				    :name "bits-per-sample"))
-	   (min-block-size-label (make-instance 'ltk:label
-						:master details-pane
-						:text "minimum block size:"))
-	   (max-block-size-label (make-instance 'ltk:label
-						:master details-pane
-						:text "maximum block size:")))
+    (let* ((button-frame (make-instance 'ltk:frame))
+           (open-flac-file-button (make-instance 'ltk:button
+                                                 :master button-frame
+                                                 :text "Open Flac File"
+                                                 :command (lambda ()
+                                                            (let ((filename (open-flac-file)))
+                                                              (when (probe-file filename)
+                                                                (ltk:wm-title ltk:*tk*
+                                                                              (format nil "FLaC Metadata Viewer: ~A"
+                                                                                      filename)))))))
+           (exit-button (make-instance 'ltk:button
+                                       :master button-frame
+                                       :text "Exit Application"
+                                       :command (lambda ()
+                                                  (return-from main nil))))
+           (details-pane (make-instance 'ltk:frame))
+           (stream-info-section-title (make-instance 'ltk:label
+                                                     :master details-pane
+                                                     :text "FLac File Stream Header Information"))
+           (bits-per-sample-label (make-instance 'ltk:label
+                                                     :master details-pane
+                                                 :text "bits per sample:"))
+           (bps-info (make-instance 'ltk:text
+                                    :master details-pane
+                                    :name "bits-per-sample"))
+           (min-block-size-label (make-instance 'ltk:label
+                                                :master details-pane
+                                                :text "minimum block size:"))
+           (min-block-size (make-instance 'ltk:text
+                                          :master details-pane
+                                          :name "min-block-size"))
+           (max-block-size-label (make-instance 'ltk:label
+                                                :master details-pane
+                                                :text "maximum block size:"))
+           (max-block-size (make-instance 'ltk:text
+                                          :master details-pane
+                                          :text "max-block-size")))
       (ltk:wm-title ltk:*tk* "Flac Metadata Viewer")
-      (ltk:pack top-frame)
-      (ltk:pack button-frame)
-      (ltk:pack open-flac-file-button :side :left :padx 30 :pady 5)
-      (ltk:pack exit-button :side :left :padx 10)
-      (ltk:configure button-frame :borderwidth 3)
-      (ltk:pack details-pane)
-      (ltk:pack stream-info-section-title :anchor :e :side :left)
-      (ltk:pack bits-per-sample-label :anchor :s :side :bottom)
-      (ltk:pack min-block-size-label :anchor :s :side :bottom)
-      (ltk:pack max-block-size-label :anchor :s :side :bottom))))
+      (ltk:grid button-frame 0 0
+                :rowspan 3
+                :columnspan 3
+                :sticky :nw
+                )
+      (ltk:grid open-flac-file-button 0 1 :sticky :w)
+      (ltk:grid exit-button 0 2 :sticky :w)
+      (ltk:grid details-pane 3 0
+                :sticky :nw
+                :rowspan 20
+                :columnspan 150)
+      (ltk:grid stream-info-section-title 3 0
+                :ipadx 10
+                :columnspan 20
+                :sticky :w)
+      (ltk:grid bits-per-sample-label 4 0
+                :ipadx 10
+                :columnspan 2
+                :sticky :w)
+      ;; (ltk:grid bps-info 4 3
+      ;;           :ipadx 10
+      ;;           :columnspan 1
+      ;;           :sticky :w)
+      (ltk:grid min-block-size-label 5 0
+                :ipadx 10
+                :columnspan 2
+                :sticky :w)
+      ;; (ltk:grid min-block-size 5 3
+      ;;           :ipadx 10
+      ;;           :columnspan 2
+      ;;           :sticky :w)
+      (ltk:grid max-block-size-label 6 0
+                :ipadx 10
+                :columnspan 2
+                :sticky :w)
+      ;; (ltk:grid max-block-size 6 3
+      ;;           :ipadx 10
+      ;;           :columnspan 2
+      ;;           :sticky :w)
+      )))
